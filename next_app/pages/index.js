22c17f1d81aa355594e3417ea3ab320235f8652c
julianhahn01@gmail.com <julianhahn01@gmail.com>
2021-10-29 11:44:34 +0200

    another push to test the webhook (2)

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Julian's next js!
+          Julian's next js, second try!
         </h1>
 
         <p className={styles.description}>
